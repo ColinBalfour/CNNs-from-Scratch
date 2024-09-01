4906cb603d9416363dc73fe0b6c9c72c801ab00f
legoeruro <lnntkhang@gmail.com>
2024-09-01 12:15:00 -0400

    Part 1: change everything to use on GPU

--- src/custom_layers.py
+++ src/custom_layers.py
@@ -1,9 +1,12 @@
 import torch
 import logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 import time
-import scipy # type: ignore
+import scipy  # type: ignore
+
+
 class CustomLinearLayer(torch.autograd.Function):
     @staticmethod
     def forward(input, weight, bias):
@@ -16,8 +19,10 @@ class CustomLinearLayer(torch.autograd.Function):
         # output=1 is a placeholder
         # print(input.shape, weight.shape, bias.shape)
         if input.dim() == 1:
-            input = input.unsqueeze(0) 
-        output = torch.mm(input, weight.T) + bias # ??? is this it lol he gave us the formula
+            input = input.unsqueeze(0)
+        output = (
+            torch.mm(input, weight.T) + bias
+        )  # ??? is this it lol he gave us the formula
 
         return output
 
@@ -32,7 +37,7 @@ class CustomLinearLayer(torch.autograd.Function):
         # Shapes.
         # grad_output - batch x output_count
         # grad_input  - batch x input
-        # grad_weight - output x input 
+        # grad_weight - output x input
         # grad_bias   - output shape
 
         input, weight, bias = ctx.saved_tensors
@@ -47,7 +52,7 @@ class CustomLinearLayer(torch.autograd.Function):
         # print("grad_input:", time.time() - t)
         # for each output y_j = sum(u_i * w.T_(j, i))
         # => dy_j/dw_(a, b) = u_b when a = j, 0 otherwise
-        
+
         # avg_weight = torch.zeros(weight.shape[0], *weight.shape).to('cuda')
 
         # "Psuedocode" - should probably vectorize/optimize this
@@ -60,9 +65,8 @@ class CustomLinearLayer(torch.autograd.Function):
         #                 tmp_grad[a, b] = input[batch, b] if a == j else 0
         #         grad_weight[j] = tmp_grad
         #     print('thjing', grad_weight)
-            
-        #     avg_weight += grad_weight
 
+        #     avg_weight += grad_weight
 
         # Too optimized version: (gpu is sad, if only we had infinite memory)
         # Shape: (batch, output, output, input)
@@ -72,24 +76,27 @@ class CustomLinearLayer(torch.autograd.Function):
         # indices = torch.arange(weight.shape[0])
         # grad_weight[:, indices, indices] = input.unsqueeze(1)
 
-
         # Goldilocks
-        avg_weight = torch.zeros(weight.shape[0], *weight.shape, device='cuda')
-        grad_weight = torch.zeros(weight.shape[0], *weight.shape, device='cuda')
-        indices = torch.arange(weight.shape[0], device='cuda')
+        avg_weight = torch.zeros(weight.shape[0], *weight.shape, device="cuda")
+        grad_weight = torch.zeros(weight.shape[0], *weight.shape, device="cuda")
+        indices = torch.arange(weight.shape[0], device="cuda")
         for batch in range(input.shape[0]):
             # Fill diagonals with input values (identity * input)
             grad_weight[indices, indices] = input[batch]
-            
+
             # Accumulate the gradients
             avg_weight += grad_weight
         # print("avg_weight:", time.time() - t)
 
-        grad_weight = avg_weight / input.shape[0] # average over batch -> Shape: (output, output, input)
-        grad_weight = torch.einsum('ij,jkl->kl', grad_output, grad_weight) # Collapse the output dimension
+        grad_weight = (
+            avg_weight / input.shape[0]
+        )  # average over batch -> Shape: (output, output, input)
+        grad_weight = torch.einsum(
+            "ij,jkl->kl", grad_output, grad_weight
+        )  # Collapse the output dimension
 
-        grad_bias = torch.einsum('ij,j->j', grad_output, torch.ones_like(bias))
-            
+        grad_bias = torch.einsum("ij,j->j", grad_output, torch.ones_like(bias))
+
         # use either print or logger to print its outputs.
         # make sure you disable before submitting
         # print(grad_input)
@@ -100,7 +107,8 @@ class CustomLinearLayer(torch.autograd.Function):
         # print("linear backprop:", time.time() - t)
 
         return grad_input, grad_weight, grad_bias
-    
+
+
 class CustomReLULayer(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input):
@@ -115,14 +123,16 @@ class CustomReLULayer(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        input, = ctx.saved_tensors
+        (input,) = ctx.saved_tensors
 
         t = time.time()
 
         grad_input = grad_output.clone()
         # YOUR IMPLEMENTATION HERE!
         # dReLU/dx = 1 if x > 0, 0 otherwise (heaviside)
-        grad_input[input < 0] = 0 # squeeze and unsqueeze because grad_output is [10], but input is [1, 10] (nvm its not needed anymore but leaving this comment here in case it decides to be funky again)
+        grad_input[input < 0] = (
+            0  # squeeze and unsqueeze because grad_output is [10], but input is [1, 10] (nvm its not needed anymore but leaving this comment here in case it decides to be funky again)
+        )
         # print("relu backprop:", time.time() - t)
         return grad_input
 
@@ -143,7 +153,7 @@ class CustomSoftmaxLayer(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        softmax_output, = ctx.saved_tensors
+        (softmax_output,) = ctx.saved_tensors
         dim = ctx.dim
 
         # YOUR IMPLEMENTATION HERE!
@@ -153,7 +163,9 @@ class CustomSoftmaxLayer(torch.autograd.Function):
         SM = softmax_output.unsqueeze(2)  # Shape: (batch_size, num_classes, 1)
         # print("dshdui")
         # print(SM.shape)
-        grad_input = torch.diag_embed(softmax_output) - torch.bmm(SM, SM.transpose(1, 2))
+        grad_input = torch.diag_embed(softmax_output) - torch.bmm(
+            SM, SM.transpose(1, 2)
+        )
         # print(grad_input.shape, grad_output.shape)
         # grad_input = torch.einsum('bi,bij->bj', grad_output, grad_input)
         grad_input = torch.bmm(grad_input, grad_output.unsqueeze(2)).squeeze(2)
@@ -163,17 +175,18 @@ class CustomSoftmaxLayer(torch.autograd.Function):
         # print(grad_input.shape)
         return grad_input, None
 
+
 class CustomConvLayer(torch.autograd.Function):
     @staticmethod
     def forward(input, weight, bias, stride, kernel_size):
         # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
         # implement the cross correlation filter
-        
+
         # weight shape - out_ch x in_ch x kernel_width x kernel_height
         # bias shape - out_ch
         # input shape - batch x ch x width x height
         # out shape - batch x out_ch x width //stride x height //stride
-        
+
         # You can assume the following,
         #  no padding
         #  kernel width == kernel height
@@ -183,22 +196,24 @@ class CustomConvLayer(torch.autograd.Function):
         in_ch = weight.shape[1]
 
         kernel = kernel_size
-        
+
         batch, _, height, width = input.shape
 
         # YOUR IMPLEMENTATION HERE!
         output = torch.zeros(batch, out_ch, width // stride, height // stride)
         for i in range(batch):
             for j in range(out_ch):
-
+                pass
         return output
-    
+
     @staticmethod
     def cross_correlate(input, kernel, stride):
         out = np.zeros((input.shape[0] // stride, input.shape[1] // stride))
         for i in range(1, input.shape[0] - 1, stride):
             for j in range(1, input.shape[1] - 1, stride):
-                filtered_image[i-1, j-1] = np.sum(img[i-1:i+2, j-1:j+2] * kernel)
+                filtered_image[i - 1, j - 1] = np.sum(
+                    img[i - 1 : i + 2, j - 1 : j + 2] * kernel
+                )
 
     @staticmethod
     def setup_context(ctx, inputs, output):
@@ -222,7 +237,7 @@ class CustomConvLayer(torch.autograd.Function):
         in_ch = weight.shape[1]
 
         kernel = weight.shape[2]
-        
+
         batch, _, height, width = input.shape
 
         # YOUR IMPLEMENTATION HERE!
